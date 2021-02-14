f2fd6683177010c3f07bd2450e7036a42703b43c
Kausik N <nkausik1999@gmail.com>
2021-02-14 17:07:41 +0530

    Update Q3.py

--- Assignment1/Codes/Q3.py
+++ Assignment1/Codes/Q3.py
@@ -10,6 +10,51 @@ import numpy as np
 def ReadFile(path):
     return open(path, 'r').read()
 
+def GetWordFreq(text):
+
+    # Get Words
+    lines = text.split('.')
+    words = []
+    for l in lines:
+        l = l.strip().lower()
+        words.extend(l.split(' '))
+
+    for i in range(len(words)):
+        words[i] = words[i].strip()
+
+    # Count
+    uniqueWords = list(set(words))
+    freq = {}
+    for w in uniqueWords:
+        freq[w] = 0
+
+    for w in words:
+        freq[w] += 1
+
+    return freq
+
+def GetCombinedFreqVector(texts):
+    # Convert Text to frequency list
+    Fs = []
+    totalWords = set([])
+    for text in texts:
+        Fs.append(GetWordFreq(text))
+        totalWords = totalWords.union(set(Fs[-1].keys()))
+    totalWords = list(totalWords)
+
+    Ps = []
+    for i in range(len(Fs)):
+        Ps.append([])
+        for w in totalWords:
+            curFreq = 0
+            if w in Fs[i].keys():
+                curFreq = Fs[i][w]
+            else:
+                curFreq = 0
+            Ps[-1].append(curFreq)
+
+    return Ps, totalWords
+
 def CosineDistance(P1, P2):
     dist = 0
 
@@ -43,10 +88,25 @@ print("Doc 2 (" + str(len(doc2_Text)) + " chars, " + str(len(doc2_Text.split(' '
 print("")
 
 # Cosine Distance
-# Convert Text to ASCII list
-P1 = list(map(ord, doc1_Text))
-P2 = list(map(ord, doc2_Text))
+# # Convert Text to ASCII list
+# P1 = list(map(ord, doc1_Text))
+# P2 = list(map(ord, doc2_Text))
+
+# Convert Text to Freq Lists
+Ps, totalWords = GetCombinedFreqVector([doc1_Text, doc2_Text])
+
+print("All words in documents:")
+print(totalWords)
+print()
+
+print("Document 1 Vector:")
+print(Ps[0])
+print()
+
+print("Document 2 Vector:")
+print(Ps[1])
+print()
 
 # Compute Cosine Distance
-CosineDist = CosineDistance(P1, P2)
+CosineDist = CosineDistance(Ps[0], Ps[1])
 print("Cosine Distance:", CosineDist)
